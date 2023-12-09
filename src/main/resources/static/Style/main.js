d5d9bf9a4e71aed3d9291126a330e6b16ab7daff
thanhITSW <nguyenmtthanh13@gmail.com>
2023-12-09 15:47:07 +0700

    edit temple

--- src/main/resources/static/Style/main.js
+++ src/main/resources/static/Style/main.js
@@ -55,24 +55,26 @@ $(".profile").click(function () {
 
 //=================================================================================================Product-item JS
 let preveiwContainer = document.querySelector(".products-preview");
-let previewBox = preveiwContainer.querySelectorAll(".preview");
+if(preveiwContainer) {
+  let previewBox = preveiwContainer.querySelectorAll(".preview");
 
-document.querySelectorAll(".products-container .product").forEach((product) => {
-  product.onclick = () => {
-    preveiwContainer.style.display = "flex";
-    let name = product.getAttribute("data-name");
-    previewBox.forEach((preview) => {
-      let target = preview.getAttribute("data-target");
-      if (name == target) {
-        preview.classList.add("active");
-      }
-    });
-  };
-});
-
-previewBox.forEach((close) => {
-  close.querySelector(".fa-times").onclick = () => {
-    close.classList.remove("active");
-    preveiwContainer.style.display = "none";
-  };
-});
+  document.querySelectorAll(".products-container .product").forEach((product) => {
+    product.onclick = () => {
+      preveiwContainer.style.display = "flex";
+      let name = product.getAttribute("data-name");
+      previewBox.forEach((preview) => {
+        let target = preview.getAttribute("data-target");
+        if (name == target) {
+          preview.classList.add("active");
+        }
+      });
+    };
+  });
+  
+  previewBox.forEach((close) => {
+    close.querySelector(".fa-times").onclick = () => {
+      close.classList.remove("active");
+      preveiwContainer.style.display = "none";
+    };
+  });
+}
